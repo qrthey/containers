ce750817119c91cb81d6c4e6ac0f4c5016d0c0ec
qrt <kh@sharpthoughts.net>
2019-06-22 21:51:06 +0200

    Fix wrong reference to docker image name

--- da-pg/run.sh
+++ da-pg/run.sh
@@ -7,9 +7,9 @@ mkdir -p $volume_host_folder
 
 ls Dockerfile && \
     docker build -t da-pg . && \
-    docker run -d --rm --name "da-pg-$1" -p 127.0.0.1:5432:5432 -v "$volume_host_folder:/var/lib/postgresql/data" qrt-da-pg && \
+    docker run -d --rm --name "da-pg-$1" -p 127.0.0.1:5432:5432 -v "$volume_host_folder:/var/lib/postgresql/data" da-pg && \
     ../wait-for-it.sh localhost:5432 && \
-    sleep 1 && \
-    docker run -it --rm --name "da-pg-$1-client" --network=host qrt-da-pg psql -h localhost -U postgres
+    sleep 3 && \
+    docker run -it --rm --name "da-pg-$1-client" --network=host da-pg psql -h localhost -U postgres
 
 docker stop "da-pg-$1"
